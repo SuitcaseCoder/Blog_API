6de3a63ad6111417592137bd67ea3dbb3b1579e8
Laura Ruiz-Roehrs <lauraruizroehrs@gmail.com>
2019-02-21 14:22:24 -0600

    updating

---
+++ blog-posts.js
@@ -0,0 +1,12 @@
+const Blog = [
+  {
+    "title":"Chaz",
+    "content":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "author":"Laura"
+  },
+  {
+    "title":"Fritz",
+    "content":"Fritz was the greatest dog that ever was",
+    "author":"mimo"
+  }
+];
--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express');
 const queryString = require('query-string');
 
 const app = express();
+
+const blogPostRouter = require('./blog-posts');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + '/views/index.html');
+});
